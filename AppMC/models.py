0f4282436eca2ee4c08fa35d2c18ef43cf95ceef
JohanMorenoT <114170704+JohanMorenoT@users.noreply.github.com>
2023-03-29 21:42:07 -0500

    cambios

--- AppMC/models.py
+++ AppMC/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     apellidos = models.CharField(max_length=100)
     telefono = models.CharField(max_length=20)
     direccion = models.CharField(max_length=200)
-    image = models.ImageField(upload_to="usuarios", null=True, blank=True)
+    image = models.ImageField(default='usuario.png' ,upload_to="usuarios", null=True, blank=True)
 
     def __str__(self):
         return f'Perfil de {self.user.username}'
