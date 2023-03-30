b3f40139fecf9ff934fc03603df1868b27730c61
JohanMorenoT <114170704+JohanMorenoT@users.noreply.github.com>
2023-03-30 10:17:33 -0500

    Detalles del producto

--- AppMC/urls.py
+++ AppMC/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import edit_profile, profileimage
+from .views import edit_profile, profileimage, post_detail
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,7 +8,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', views.index, name='index'),
     path('feed/', views.feed, name='feed'),
-    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('product/<int:post_id>/', post_detail, name='product_detail'),
     path('profile/', views.profile, name='profile'),
     path('perfil/editar/', edit_profile, name='editar_perfil'),
     path('perfil/editarimagen/', profileimage, name='editar_imagen'),
--- AppMC/views.py
+++ AppMC/views.py
@@ -44,8 +44,8 @@ def post(request):
     return render(request, 'social/post.html', {'form' : form})
 
 def post_detail(request, post_id):
-    post = get_object_or_404(Post, post_id=post_id)
-    return render(request, 'post_detail.html', {'post': post})
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'social/post_detail.html', {'post': post})
 
 def profile(request, username=None):
     current_user = request.user
